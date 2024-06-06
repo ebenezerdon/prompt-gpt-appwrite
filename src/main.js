3b3400be5801b904770d096cc1b02c1d35d2a4f8
Ebenezer Don <ebenezerdonu@gmail.com>
2024-06-06 17:18:54 +0100

    Update Prettier configuration and remove unnecessary semicolons

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
-import OpenAI from 'openai';
-import { getStaticFile, throwIfMissing } from './utils.js';
+import OpenAI from 'openai'
+import { getStaticFile, throwIfMissing } from './utils.js'
 
 export default async ({ req, res }) => {
-  const { method, body } = req;
-  const { send, json } = res;
+  throwIfMissing(process.env, ['OPENAI_API_KEY'])
 
-  throwIfMissing(process.env, ['OPENAI_API_KEY']);
-
-  if (method === 'GET') {
-    return send(getStaticFile('index.html'), 200, {
+  if (req.method === 'GET') {
+    return res.send(getStaticFile('index.html'), 200, {
       'Content-Type': 'text/html; charset=utf-8',
-    });
+    })
   }
 
   try {
-    throwIfMissing(body, ['prompt']);
+    throwIfMissing(req.body, ['prompt'])
   } catch (err) {
-    return json({ ok: false, error: err.message }, 400);
+    return res.json({ ok: false, error: err.message }, 400)
   }
 
-  const openai = new OpenAI();
+  const openai = new OpenAI()
 
   try {
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? '512'),
-      messages: [{ role: 'user', content: body.prompt }],
-    });
-    const completion = response.choices[0]?.message?.content;
-    return json({ ok: true, completion }, 200);
+      messages: [{ role: 'user', content: req.body.prompt }],
+    })
+    const completion = response.choices[0].message.content
+    return res.json({ ok: true, completion }, 200)
   } catch (err) {
-    return json({ ok: false, error: err.message }, 500);
+    return res.json({ ok: false, error: err.message }, 500)
   }
-};
+}
