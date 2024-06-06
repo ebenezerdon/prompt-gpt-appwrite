48d9c2b66c7c80e9a89d2e3eb941c836577ca212
Ebenezer Don <ebenezerdonu@gmail.com>
2024-06-06 17:10:24 +0100

    chore: minor optimization

--- src/main.js
+++ src/main.js
@@ -2,18 +2,21 @@ import OpenAI from 'openai';
 import { getStaticFile, throwIfMissing } from './utils.js';
 
 export default async ({ req, res }) => {
+  const { method, body } = req;
+  const { send, json } = res;
+
   throwIfMissing(process.env, ['OPENAI_API_KEY']);
 
-  if (req.method === 'GET') {
-    return res.send(getStaticFile('index.html'), 200, {
+  if (method === 'GET') {
+    return send(getStaticFile('index.html'), 200, {
       'Content-Type': 'text/html; charset=utf-8',
     });
   }
 
   try {
-    throwIfMissing(req.body, ['prompt']);
+    throwIfMissing(body, ['prompt']);
   } catch (err) {
-    return res.json({ ok: false, error: err.message }, 400);
+    return json({ ok: false, error: err.message }, 400);
   }
 
   const openai = new OpenAI();
@@ -22,17 +25,11 @@ export default async ({ req, res }) => {
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? '512'),
-      messages: [{ role: 'user', content: req.body.prompt }],
+      messages: [{ role: 'user', content: body.prompt }],
     });
-    const completion = response.choices[0].message.content;
-    return res.json({ ok: true, completion }, 200);
+    const completion = response.choices[0]?.message?.content;
+    return json({ ok: true, completion }, 200);
   } catch (err) {
-    return res.json(
-      {
-        ok: false,
-        error: err.message,
-      },
-      500
-    );
+    return json({ ok: false, error: err.message }, 500);
   }
 };
